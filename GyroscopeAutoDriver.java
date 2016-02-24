cf0ebe6513be85aea416ebd2dc70bbdae7c8484a
JacobAMason <jm2232@msstate.edu>
2016-02-24 10:36:02 -0600

    Rename AutoDriver to GyroscopeautoDriver. Refactor it to use haltDrive

---
+++ GyroscopeAutoDriver.java
@@ -0,0 +1,109 @@
+package com.FTC3486.FTCRC_Extensions;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by Matthew on 2/20/2016.
+ */
+public class GyroscopeAutoDriver {
+    private LinearOpMode opMode;
+    private DriveTrain driveTrain;
+    private GyroSensor gyroSensor;
+    private ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+    public GyroscopeAutoDriver(LinearOpMode linearOpMode, DriveTrain driveTrain, String gyroSensor,
+                               HardwareMap hardwaremap) {
+        this.opMode = linearOpMode;
+        this.driveTrain = driveTrain;
+        this.gyroSensor = hardwaremap.gyroSensor.get(gyroSensor);
+    }
+
+    public void driveForwardtoEncoderCountWithCorrection(int encoderCount, double power,
+                                                         int gyroTarget) {
+        driveTrain.resetMotorEncoders();
+
+        while (driveTrain.getLeftEncoderCount() < encoderCount && driveTrain.getRightEncoderCount()
+                < encoderCount && opMode.opModeIsActive()) {
+            //opMode.telemetry.addData("Gyro Heading", gyroSensor.getHeading());
+            if(gyroSensor.getHeading() < gyroTarget) {
+                driveTrain.setPowers(power, power - (0.75* power));
+            } else if(gyroSensor.getHeading() > gyroTarget) {
+                driveTrain.setPowers(power - (0.75 * power) , power);
+            } else {
+                driveTrain.setPowers(power, power);
+            }
+        }
+        driveTrain.haltDrive();
+    }
+
+    public void driveBackwardtoEncoderCountWithCorrection(int encoderCount, double power,
+                                                          int gyroTarget) {
+        driveTrain.resetMotorEncoders();
+
+        while (driveTrain.getLeftEncoderCount() > encoderCount && driveTrain.getRightEncoderCount()
+                > encoderCount && opMode.opModeIsActive()) {
+            //opMode.telemetry.addData("Gyro Heading", gyroSensor.getHeading());
+            if(gyroSensor.getHeading() < gyroTarget) {
+                driveTrain.setPowers(power, power + (0.75* power));
+            } else if(gyroSensor.getHeading() > gyroTarget) {
+                driveTrain.setPowers(power + (0.75 * power) , power);
+            } else {
+                driveTrain.setPowers(power, power);
+            }
+        }
+        driveTrain.haltDrive();
+    }
+
+    public void driveBackwardtoEncoderCount(int encoderCount, double power) {
+        driveTrain.resetMotorEncoders();
+        driveTrain.setPowers(power, power);
+        while (driveTrain.getLeftEncoderCount() > encoderCount && driveTrain.getRightEncoderCount()
+                > encoderCount && opMode.opModeIsActive()) {}
+        driveTrain.haltDrive();
+    }
+
+
+    public void gyroTurn(String direction, int gyroHeading, double power) {
+        switch(direction) {
+            default:
+            case "CLOCKWISE":
+                driveTrain.setPowers(power, -power);
+
+                while(gyroSensor.getHeading() > (gyroHeading - 11) && opMode.opModeIsActive()) {
+                    //opMode.telemetry.addData("Gyro Heading", gyroSensor.getHeading());
+                }
+
+                while(gyroSensor.getHeading() < (gyroHeading - 11) && opMode.opModeIsActive()) {
+                    //opMode.telemetry.addData("Gyro Heading", gyroSensor.getHeading());
+                }
+
+                driveTrain.haltDrive();
+                break;
+
+            case "COUNTER_CLOCKWISE":
+                driveTrain.setPowers(-power, power);
+
+                while(gyroSensor.getHeading() < (gyroHeading + 11) && opMode.opModeIsActive()) {
+                    //opMode.telemetry.addData("Gyro Heading", gyroSensor.getHeading());
+                }
+
+                while(gyroSensor.getHeading() > (gyroHeading + 11) && opMode.opModeIsActive()) {
+                    //opMode.telemetry.addData("Gyro Heading", gyroSensor.getHeading());
+                }
+
+                driveTrain.haltDrive();
+                break;
+        }
+
+    }
+
+    public void waitMilliseconds(int waitTime) throws InterruptedException{
+        timer.reset();
+        while(timer.time() < waitTime && opMode.opModeIsActive()) { opMode.sleep(1); }
+    }
+
+}
+
