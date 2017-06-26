f3539b15eb9704ae8efd5893382111e9062e0fc1
mdttrump97 <mdttrump97@bellsouth.net>
2017-06-25 22:18:08 -0500

    Remodels AutoDriver to work with Robot architecture for #12

--- AutoDriver.java
+++ AutoDriver.java
@@ -19,24 +19,19 @@ public abstract class AutoDriver {
         this.drivetrain = drivetrain;
     }
 
-    public void set_power(double power) {
-        if (power < 0) throw new IllegalArgumentException("the power should always be positive");
-        this.power = power;
-    }
-
     public void set_wait_time_between_movements(int milliseconds) {
         if (milliseconds < 0) throw new IllegalArgumentException("the wait time should always be positive");
         this.wait_time_ms = milliseconds;
     }
 
-    private void setup_motion(String motion_description) {
+    protected void setup_motion(String motion_description) {
         eStop = false;
         opMode.telemetry.addData("AutoDriver", motion_description);
         drivetrain.resetMotorEncoders();
         stallMonitor.start_monitoring();
     }
 
-    private void end_motion() throws InterruptedException {
+    protected void end_motion() throws InterruptedException {
         drivetrain.haltDrive();
         stallMonitor.stop_monitoring();
         opMode.telemetry.addData("AutoDriver", "Halting");
@@ -46,37 +41,4 @@ public abstract class AutoDriver {
     protected void e_stop() {
         eStop = true;
     }
-
-    public AutoDriver drive_forward(int encoderCounts) throws InterruptedException {
-        setup_motion("Forward");
-        drive_forward_implementation(encoderCounts);
-        end_motion();
-        return this;
-    }
-
-    public AutoDriver drive_backward(int encoderCounts) throws InterruptedException {
-        setup_motion("Backward");
-        drive_backward_implementation(encoderCounts);
-        end_motion();
-        return this;
-    }
-
-    public AutoDriver turn_clockwise(int degrees) throws InterruptedException {
-        setup_motion("Clockwise");
-        turn_clockwise_implementation(degrees);
-        end_motion();
-        return this;
-    }
-
-    public AutoDriver turn_counterclockwise(int degrees) throws InterruptedException {
-        setup_motion("Counterclockwise");
-        turn_counterclockwise_implementation(degrees);
-        end_motion();
-        return this;
-    }
-
-    public abstract AutoDriver drive_forward_implementation(int encoderCounts);
-    public abstract AutoDriver drive_backward_implementation(int encoderCounts);
-    public abstract AutoDriver turn_clockwise_implementation(int degrees);
-    public abstract AutoDriver turn_counterclockwise_implementation(int degrees);
 }
