f4bb446bd106b8069ab27c6d974c4d3f8568e204
mdttrump97 <mdttrump97@bellsouth.net>
2017-06-26 21:29:53 -0500

    Changes AutoDriver to camel case and adds the hardware configuration for #12

--- AutoDriver.java
+++ AutoDriver.java
@@ -1,44 +1,40 @@
 package org.firstinspires.ftc.teamcode.RobotCoreExtensions;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 /**
  * Created by Jacob on 2/24/16.
  */
 public abstract class AutoDriver {
-    protected LinearOpMode opMode;
-    protected Drivetrain drivetrain;
+    HardwareConfiguration hw;
     private StallMonitor stallMonitor = new StallMonitor(this);
     protected double power = 1.0D;
     private int wait_time_ms = 500;
     protected boolean eStop = false;
 
 
-    public AutoDriver(LinearOpMode opMode, Drivetrain drivetrain) {
-        this.opMode = opMode;
-        this.drivetrain = drivetrain;
+    public AutoDriver(HardwareConfiguration hw) {
+        this.hw = hw;
     }
 
-    public void set_wait_time_between_movements(int milliseconds) {
+    public void setWaitTimeBetweenMovements(int milliseconds) {
         if (milliseconds < 0) throw new IllegalArgumentException("the wait time should always be positive");
         this.wait_time_ms = milliseconds;
     }
 
-    protected void setup_motion(String motion_description) {
+    protected void setupMotion(String motion_description) {
         eStop = false;
-        opMode.telemetry.addData("AutoDriver", motion_description);
-        drivetrain.resetMotorEncoders();
+        hw.opMode.telemetry.addData("AutoDriver", motion_description);
+        hw.drivetrain.resetMotorEncoders();
         stallMonitor.start_monitoring();
     }
 
-    protected void end_motion() throws InterruptedException {
-        drivetrain.haltDrive();
+    protected void endMotion() throws InterruptedException {
+        hw.drivetrain.haltDrive();
         stallMonitor.stop_monitoring();
-        opMode.telemetry.addData("AutoDriver", "Halting");
-        opMode.sleep(wait_time_ms);
+        hw.opMode.telemetry.addData("AutoDriver", "Halting");
+        hw.opMode.sleep(wait_time_ms);
     }
 
-    protected void e_stop() {
+    protected void eStop() {
         eStop = true;
     }
 }
