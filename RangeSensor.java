26e2217638f8ecdf7b186b502cbcfc6140902f04
mdttrump97 <mdttrump97@bellsouth.net>
2017-07-21 23:52:03 -0500

    Adds documentation to RangeSensor class

--- RangeSensor.java
+++ RangeSensor.java
@@ -11,6 +11,25 @@ import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
  * Created by Matthew on 3/10/2017.
  */
 
+/*
+    Filename: RangeSensor.java
+
+    Description:
+        This Range Sensor class allows the robot to access two different range sensors at once. The
+    original range sensor code wrote the data from both sensors to the same location, so it would not
+    allow for two different sensors to be used at the same time. This class solves this problem by
+    storing the range sensor data in specified locations (at the "address" specified in the constructor).
+
+    Use:
+        The Range Sensor class is used in the hardware configuration to create range sensors for the
+    robot to use in a teleop or autonomous setting. A range sensor object should be created in the
+    hardware configuration and accessed from a teleop file using an associated robot object. For
+    example, type robot.hardwareConfiguration.rangeSensor.getUltraSonicRange() (using the specific
+    robot class, hardware configuration, and rangeSensor objects) to access the data from a specific
+    sensor.
+ */
+
+
 public class RangeSensor {
     private I2cDeviceSynch reader;
 
