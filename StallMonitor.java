5b03873104ad41d2bbf688c340bd0af48f7d349f
mdttrump97 <mdttrump97@bellsouth.net>
2017-07-21 22:16:03 -0500

    Adds file description documentation to Stall Monitor

--- StallMonitor.java
+++ StallMonitor.java
@@ -6,6 +6,24 @@ import java.util.TimerTask;
 /**
  * Created by Jacob on 4/6/16.
  */
+
+/*
+    Filename: StallMonitor.java
+
+    Description:
+        Stall Monitor forms a crucial piece of the autonomous code for robot movement each season.
+    Stall Monitor monitors the motor encoders of each drive motor during movements, and it stops the
+    motors if the robot is trying to drive but not moving far enough. The robot checks its encoder
+    counts over a time increment and stops the robot if the change in encoder counts is too low. This
+    could occur when the robot collides with another object, and it protects our drive motors from
+    possible damage from autonomous collisions.
+
+    Use:
+        Stall Monitor is used by each AutoDriver when any movement function is called in an autonomous
+    program. This ensures that every predefined movement (squaring up to a line, driving with the
+    gyro sensor, etc.) is being monitored.
+ */
+
 class StallMonitor {
     AutoDriver autoDriver;
     Timer stallTimer = new Timer();
