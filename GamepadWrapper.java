fd51915cc13ea0117a721828bc9ba95c04c8e549
JacobAMason <jacob@jacobmason.net>
2017-12-28 11:52:48 -0600

    Update GamepadWrapper with onPress ButtonMap
    Signed-off-by: JacobAMason <jacob@jacobmason.net>

--- GamepadWrapper.java
+++ GamepadWrapper.java
@@ -27,156 +27,179 @@ public class GamepadWrapper
     }
 
     public ButtonMap toggle = new ButtonMap();
+    public ButtonMap onPress = new ButtonMap();
     private ButtonMap previousButtonStates = new ButtonMap();
 
-    private void updateButtonMap(ButtonMap map, Gamepad gamepad)
+    private void savePreviousButtonStates(Gamepad gamepad)
     {
-        map.dpad_up = gamepad.dpad_up;
-        map.dpad_down = gamepad.dpad_down;
-        map.dpad_right = gamepad.dpad_right;
-        map.dpad_left = gamepad.dpad_left;
-        map.a = gamepad.a;
-        map.b = gamepad.b;
-        map.x = gamepad.x;
-        map.y = gamepad.y;
-        map.start = gamepad.start;
-        map.back = gamepad.back;
-        map.right_bumper = gamepad.right_bumper;
-        map.left_bumper = gamepad.left_bumper;
-        map.left_stick_button = gamepad.left_stick_button;
-        map.right_stick_button = gamepad.right_stick_button;
+        previousButtonStates.dpad_up = gamepad.dpad_up;
+        previousButtonStates.dpad_down = gamepad.dpad_down;
+        previousButtonStates.dpad_right = gamepad.dpad_right;
+        previousButtonStates.dpad_left = gamepad.dpad_left;
+        previousButtonStates.a = gamepad.a;
+        previousButtonStates.b = gamepad.b;
+        previousButtonStates.x = gamepad.x;
+        previousButtonStates.y = gamepad.y;
+        previousButtonStates.guide = gamepad.guide;
+        previousButtonStates.start = gamepad.start;
+        previousButtonStates.back = gamepad.back;
+        previousButtonStates.right_bumper = gamepad.right_bumper;
+        previousButtonStates.left_bumper = gamepad.left_bumper;
+        previousButtonStates.left_stick_button = gamepad.left_stick_button;
+        previousButtonStates.right_stick_button = gamepad.right_stick_button;
     }
     
     public void update(Gamepad gamepad)
     {
-        if (!gamepad.dpad_up && previousButtonStates.dpad_up)
+        onPress.dpad_up = (gamepad.dpad_up && !previousButtonStates.dpad_up);
+        onPress.dpad_down = (gamepad.dpad_down && !previousButtonStates.dpad_down);
+        onPress.dpad_left = (gamepad.dpad_left && !previousButtonStates.dpad_left);
+        onPress.dpad_right = (gamepad.dpad_right && !previousButtonStates.dpad_right);
+        onPress.a = (gamepad.a && !previousButtonStates.a);
+        onPress.b = (gamepad.b && !previousButtonStates.b);
+        onPress.x = (gamepad.x && !previousButtonStates.x);
+        onPress.y = (gamepad.y && !previousButtonStates.y);
+        onPress.guide = (gamepad.guide && !previousButtonStates.guide);
+        onPress.start = (gamepad.start && !previousButtonStates.start);
+        onPress.back = (gamepad.back && !previousButtonStates.back);
+        onPress.right_bumper = (gamepad.right_bumper && !previousButtonStates.right_bumper);
+        onPress.left_bumper = (gamepad.left_bumper && !previousButtonStates.left_bumper);
+        onPress.left_stick_button = (gamepad.left_stick_button && !previousButtonStates.left_stick_button);
+        onPress.right_stick_button = (gamepad.right_stick_button && !previousButtonStates.right_stick_button);
+
+        updateToggleButtonStates();
+
+        savePreviousButtonStates(gamepad);
+    }
+
+    private void updateToggleButtonStates() {
+        if (onPress.dpad_up)
         {
             toggle.dpad_up = !toggle.dpad_up;
         }
-        if (!gamepad.dpad_down && previousButtonStates.dpad_down)
+        if (onPress.dpad_down)
         {
             toggle.dpad_down = !toggle.dpad_down;
         }
-        if (!gamepad.dpad_left && previousButtonStates.dpad_left)
+        if (onPress.dpad_left)
         {
             toggle.dpad_left = !toggle.dpad_left;
         }
-        if (!gamepad.dpad_right && previousButtonStates.dpad_right)
+        if (onPress.dpad_right)
         {
             toggle.dpad_right = !toggle.dpad_right;
         }
-        if (!gamepad.a && previousButtonStates.a)
+        if (onPress.a)
         {
             toggle.a = !toggle.a;
         }
-        if (!gamepad.b && previousButtonStates.b)
+        if (onPress.b)
         {
             toggle.b = !toggle.b;
         }
-        if (!gamepad.x && previousButtonStates.x)
+        if (onPress.x)
         {
             toggle.x = !toggle.x;
         }
-        if (!gamepad.y && previousButtonStates.y)
+        if (onPress.y)
         {
             toggle.y = !toggle.y;
         }
-        if (!gamepad.guide && previousButtonStates.guide)
+        if (onPress.guide)
         {
             toggle.guide = !toggle.guide;
         }
-        if (!gamepad.start && previousButtonStates.start)
+        if (onPress.start)
         {
             toggle.start = !toggle.start;
         }
-        if (!gamepad.back && previousButtonStates.back)
+        if (onPress.back)
         {
             toggle.back = !toggle.back;
         }
-        if (!gamepad.right_bumper && previousButtonStates.right_bumper)
+        if (onPress.right_bumper)
         {
             toggle.right_bumper = !toggle.right_bumper;
         }
-        if (!gamepad.left_bumper && previousButtonStates.left_bumper)
+        if (onPress.left_bumper)
         {
             toggle.left_bumper = !toggle.left_bumper;
         }
-        if (!gamepad.left_stick_button && previousButtonStates.left_stick_button)
+        if (onPress.left_stick_button)
         {
             toggle.left_stick_button = !toggle.left_stick_button;
         }
-        if (!gamepad.right_stick_button && previousButtonStates.right_stick_button)
+        if (onPress.right_stick_button)
         {
             toggle.right_stick_button = !toggle.right_stick_button;
         }
-
-        updateButtonMap(previousButtonStates, gamepad);
     }
 
     @Override
     public String toString()
     {
-        String toggleStr = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
         if(toggle.dpad_up) {
-            toggleStr = toggleStr + "tog.dpad_up ";
+            stringBuilder.append("tog.dpad_up ");
         }
 
         if(toggle.dpad_down) {
-            toggleStr = toggleStr + "tog.dpad_down ";
+            stringBuilder.append("tog.dpad_down ");
         }
 
         if(toggle.dpad_left) {
-            toggleStr = toggleStr + "tog.dpad_left ";
+            stringBuilder.append("tog.dpad_left ");
         }
 
         if(toggle.dpad_right) {
-            toggleStr = toggleStr + "tog.dpad_right ";
+            stringBuilder.append("tog.dpad_right ");
         }
 
         if(toggle.a) {
-            toggleStr = toggleStr + "tog.a ";
+            stringBuilder.append("tog.a ");
         }
 
         if(toggle.b) {
-            toggleStr = toggleStr + "tog.b ";
+            stringBuilder.append("tog.b ");
         }
 
         if(toggle.x) {
-            toggleStr = toggleStr + "tog.x ";
+            stringBuilder.append("tog.x ");
         }
 
         if(toggle.y) {
-            toggleStr = toggleStr + "tog.y ";
+            stringBuilder.append("tog.y ");
         }
 
         if(toggle.guide) {
-            toggleStr = toggleStr + "tog.guide ";
+            stringBuilder.append("tog.guide ");
         }
 
         if(toggle.start) {
-            toggleStr = toggleStr + "tog.start ";
+            stringBuilder.append("tog.start ");
         }
 
         if(toggle.back) {
-            toggleStr = toggleStr + "tog.back ";
+            stringBuilder.append("tog.back ");
         }
 
         if(toggle.left_bumper) {
-            toggleStr = toggleStr + "tog.left_bumper ";
+            stringBuilder.append("tog.left_bumper ");
         }
 
         if(toggle.right_bumper) {
-            toggleStr = toggleStr + "tog.right_bumper ";
+            stringBuilder.append("tog.right_bumper ");
         }
 
         if(toggle.left_stick_button) {
-            toggleStr = toggleStr +  "tog.left_stick_button ";
+            stringBuilder.append("tog.left_stick_button ");
         }
 
         if(toggle.right_stick_button) {
-            toggleStr = toggleStr +  "toggle.right_stick_button";
+            stringBuilder.append("toggle.right_stick_button");
         }
 
-        return toggleStr;
+        return stringBuilder.toString();
     }
 }
